e19f5b35deaa800b26caf96c623faa516038b001
trampboy <honggangjing@gmail.com>
2017-08-09 23:24:00 +0800

    TODO:新增备注，例如"COMMENT '主键 ID'"

--- crawler/database/db.js
+++ crawler/database/db.js
@@ -33,6 +33,7 @@ function query(sql, cb) {
 	});
 }
 
+//TODO 新增备注，例如"COMMENT '主键 ID'"
 function createTables() {
 	let dropdbs  = ['drop table if exists playlist163','drop table if exists music163','drop table if exists comment163'];
 	let playlist = 'CREATE TABLE `playlist163` (`id` int(11) NOT NULL AUTO_INCREMENT,`title` varchar(150) DEFAULT \'\',`link` varchar(120) DEFAULT \'\',`create_time` datetime DEFAULT CURRENT_TIMESTAMP,`over` varchar(20) DEFAULT \'N\',PRIMARY KEY (`id`),KEY `over_link` (`over`,`link`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4';
